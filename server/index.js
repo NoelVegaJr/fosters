a83d5180a26be0de3055b841c7781f1b16503ac6
NoelVega126 <noel.vega@relativity.com>
2022-08-25 23:44:51 -0500

    intital user model

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const port = 3001
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
-const mongoURI = 'mongodb+srv://noel:Spring120894$@fosters.duamo.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://noel:Spring120894$@fosters.duamo.mongodb.net/fosters?retryWrites=true&w=majority'
 const mongoose = require('mongoose');
 
 async function mongooseConnection(URI) {
@@ -14,16 +14,38 @@ async function mongooseConnection(URI) {
   } catch(error) {
     console.error(error);
   }
-  mongoose.disconnect();
 }
-
+const UserModel = require('./models/User');
 mongooseConnection(mongoURI);
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("hello world");
 })
 
+app.post('/register', async (req, res) => {
+  const {username, firstName, lastName, email, phone, password} = req.body;
+  // Checking if user exists
+  if(await UserModel.findOne({email})){
+    res.send("User already exists");
+  }
+  
+  // User does not exist. Begin creating new user.
+  let newUser = new UserModel({
+    username,
+    firstName,
+    lastName,
+    email,
+    phone,
+    password
+  })
+  await newUser.save();
+  res.send("creating users")
 
+});
 
 
 app.listen(port, ()=> {
